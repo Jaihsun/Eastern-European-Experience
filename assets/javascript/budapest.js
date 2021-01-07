a0ef4a1b63482028096d7af5ed3363f2bafdd7ff
Jaihsun <johnjasonkearney@gmail.com>
2021-01-07 16:19:20 +0000

    javascript folder added with js files

---
+++ assets/javascript/budapest.js
@@ -0,0 +1,96 @@
+
+
+    var map;
+        var InforObj = [];
+        var centerCords = {
+            lat: 47.50087,         
+            lng: 19.04392
+        };      
+
+        var markersOnMap = [{
+                placeName: "Hungarian Parliament",
+                LatLng: [{
+                    lat: 47.50765, 
+                    lng: 19.04560
+                }]
+            },
+            {
+
+                placeName: "Fishermans Bastion",
+                LatLng: [{
+                    lat: 47.50241,
+                    lng: 19.03476
+                }]
+            },
+            {
+                placeName: "St Stephen Basilica",
+                LatLng: [{
+                    lat: 47.50105,
+                    lng: 19.05403
+                }]
+            },
+            {
+                placeName: "Buda Castle",
+                LatLng: [{
+                    lat:  47.49635,
+                    lng:  19.03958
+                }]
+            },
+        
+        ];
+
+        window.onload = function () {
+            initMap();
+        };
+
+        function addMarkerInfo() {
+            for (var i = 0; i < markersOnMap.length; i++) {
+                var contentString = '<div id="content"><h1>' + markersOnMap[i].placeName +
+                    '</h1></div>';
+
+                const marker = new google.maps.Marker({
+                    position: markersOnMap[i].LatLng[0],
+                    map: map
+                });
+
+                const infowindow = new google.maps.InfoWindow({
+                    content: contentString,
+                    maxWidth: 200
+                });
+
+                marker.addListener('click', function () {
+                    closeOtherInfo();
+                    infowindow.open(marker.get('map'), marker);
+                    InforObj[0] = infowindow;
+                });
+                // marker.addListener('mouseover', function () {
+                //     closeOtherInfo();
+                //     infowindow.open(marker.get('map'), marker);
+                //     InforObj[0] = infowindow;
+                // });
+                // marker.addListener('mouseout', function () {
+                //     closeOtherInfo();
+                //     infowindow.close();
+                //     InforObj[0] = infowindow;
+                // });
+            }
+        }
+
+        function closeOtherInfo() {
+            if (InforObj.length > 0) {
+                /* detach the info-window from the marker ... undocumented in the API docs */
+                InforObj[0].set("marker", null);
+                /* and close it */
+                InforObj[0].close();
+                /* blank the array */
+                InforObj.length = 0;
+            }
+        }
+
+        function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 14,
+                center: centerCords
+            });
+            addMarkerInfo();
+        }
---
+++ assets/javascript/krakow.js
@@ -0,0 +1,97 @@
+
+    var map;
+        var InforObj = [];
+        var centerCords = {
+            lat: 50.06303,         
+            lng: 19.94137
+        };     
+        
+        var markersOnMap = [{
+                placeName: "St Florian's Gate",
+                LatLng: [{
+                    lat: 50.06503, 
+                    lng: 19.94137
+                }]
+            },
+            {
+
+                placeName: "Cloth Hall",
+                LatLng: [{
+                    lat: 50.06181,
+                    lng: 19.93742
+                }]
+            },
+            {
+                placeName: "St Marys Basilica",
+                LatLng: [{
+                    lat: 50.06180,
+                    lng: 19.93940
+                }]
+            },
+            {
+                placeName: "Old Town",
+                LatLng: [{
+                    lat: 50.05918,
+                    lng: 19.93739
+                }]
+            },
+        
+        ];
+
+        window.onload = function () {
+            initMap();
+        };
+
+        function addMarkerInfo() {
+            for (var i = 0; i < markersOnMap.length; i++) {
+                var contentString = '<div id="content"><h1>' + markersOnMap[i].placeName +
+                    '</h1></div>';
+
+                const marker = new google.maps.Marker({
+                    position: markersOnMap[i].LatLng[0],
+                    map: map
+                });
+
+                const infowindow = new google.maps.InfoWindow({
+                    content: contentString,
+                    maxWidth: 200
+                });
+
+                marker.addListener('click', function () {
+                    closeOtherInfo();
+                    infowindow.open(marker.get('map'), marker);
+                    InforObj[0] = infowindow;
+                });
+                // marker.addListener('mouseover', function () {
+                //     closeOtherInfo();
+                //     infowindow.open(marker.get('map'), marker);
+                //     InforObj[0] = infowindow;
+                // });
+                // marker.addListener('mouseout', function () {
+                //     closeOtherInfo();
+                //     infowindow.close();
+                //     InforObj[0] = infowindow;
+                // });
+            }
+        }
+
+        function closeOtherInfo() {
+            if (InforObj.length > 0) {
+                /* detach the info-window from the marker ... undocumented in the API docs */
+                InforObj[0].set("marker", null);
+                /* and close it */
+                InforObj[0].close();
+                /* blank the array */
+                InforObj.length = 0;
+            }
+        }
+
+        function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 15,
+                center: centerCords
+            });
+            addMarkerInfo();
+        }
+        
+
