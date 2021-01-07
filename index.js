cbb7613e6fc3874787aae181cf8cb9252da59856
Jaihsun <johnjasonkearney@gmail.com>
2021-01-07 00:04:18 +0000

    emailjs feature added to the contact.html

---
+++ index.js
@@ -0,0 +1,12 @@
+function sendMail (params) {
+    var tempParams = {
+        from_name: document.gtElementById("fromName").value,
+        to_name: document.gtElementById("toName").value,
+        message: document.gtElementById("msg").value,
+    };
+
+    emailjs.send('gmail','template_jvyd3tf',tempParam )
+    .then(function(res){
+        console.log("success", res.status);
+    })
+}
