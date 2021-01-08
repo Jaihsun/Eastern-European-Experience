48307c36c61e0182358fc0747267067e8c8e5857
Jaihsun <johnjasonkearney@gmail.com>
2021-01-08 00:24:46 +0000

    javascript files formatted

--- assets/javascript/kiev.js
+++ assets/javascript/kiev.js
@@ -1,45 +1,92 @@
-let map;
-let InforObj = [];
-const centerCords = {    
-         lat: 50.4551,
-        lng: 30.5234
-        };
+var map;
+var InforObj = [];
+var centerCords = {    
+    lat: 50.4551,
+    lng: 30.5234
+    };
 
-        var markersOnMap = [{
-                placeName: "Kiev (St Michaels Golden Dome)",
-                LatLng: [{
-                    lat: 50.45600, 
-                    lng: 30.52303
+var markersOnMap = [{
+placeName: "Kiev (St Michaels Golden Dome)",
+LatLng: [{
+        lat: 50.45600, 
+        lng: 30.52303
+            }]
+    },
+            
+    
+{placeName: "Saint Sophia's Cathedral",
+LatLng: [{
+    lat: 50.45311,
+    lng: 30.51434
+    }]
+    },
+            
+    
+{placeName: "Independence Square",
+    LatLng: [{
+        lat: 50.45069,
+        lng: 30.52298
                 }]
             },
-            {
 
-                placeName: "Saint Sophia's Cathedral",
-                LatLng: [{
-                    lat: 50.45311,
-                    lng: 30.51434
-                }]
-            },
-            {
-                placeName: "Independence Square",
-                LatLng: [{
-                    lat: 50.45069,
-                    lng: 30.52298
-                }]
-            },
-            {
-                placeName: "St. Andrew's Church",
-                LatLng: [{
-                    lat: 50.45919,
-                    lng: 30.51794
-                }]
-            },
+
+{placeName: "St. Andrew's Church",
+    LatLng: [{
+        lat: 50.45919,
+        lng: 30.51794
+    }]
+    },
         
-        ];
+];
 
-        
-$("first").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".second").offset().top},
-        'slow');
-});
+
+
+window.onload = function () {
+    initMap();
+    };
+
+
+function addMarkerInfo() {
+    for (var i = 0; i < markersOnMap.length; i++) {
+        var contentString = '<div id="content"><h1>' + markersOnMap[i].placeName +
+        '</h1></div>';
+
+const marker = new google.maps.Marker({
+    position: markersOnMap[i].LatLng[0],
+    map: map
+    });
+
+const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+    maxWidth: 200
+    });
+
+marker.addListener('click', function () {
+    closeOtherInfo();
+    infowindow.open(marker.get('map'), marker);
+    InforObj[0] = infowindow;
+    });
+                
+}
+}
+
+function closeOtherInfo() {
+    if (InforObj.length > 0) {
+     /* detach the info-window from the marker ... undocumented in the API docs */
+    InforObj[0].set("marker", null);
+    /* and close it */
+    InforObj[0].close();
+    /* blank the array */
+    InforObj.length = 0;
+}
+}
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 14,
+        center: centerCords
+    });
+        addMarkerInfo();
+}
+
+
--- assets/javascript/prague.js
+++ assets/javascript/prague.js
@@ -1,90 +1,94 @@
 var map;
 var InforObj = [];
 var centerCords = {
-    
-            zoom: 50,
-            lat: 50.08900,         
-            lng: 14.41141
-        };
+    zoom: 50,
+        lat: 50.08900,         
+        lng: 14.41141
+    };
    
 
-        var markersOnMap = [{
-                placeName: "Prague Castle",
-                LatLng: [{
-                    lat: 50.09150, 
-                    lng: 14.40151
-                }]
-            },
-            {
+var markersOnMap = [{
+    placeName: "Prague Castle",
+        LatLng: [{
+            lat: 50.09150, 
+            lng: 14.40151
+    }]
+    },
 
-                placeName: "Astronomical Clock",
-                LatLng: [{
-                    lat: 50.08770,
-                    lng: 14.42079
-                }]
-            },
-            {
-                placeName: "Victorious Curch",
-                LatLng: [{
-                    lat: 50.08612,
-                    lng: 14.40337
-                }]
-            },
-            {
-                placeName: "Charles Bridge",
-                LatLng: [{
-                    lat: 50.08675,
-                    lng: 14.41141
-                }]
-            },
+
+{
+    placeName: "Astronomical Clock",
+    LatLng: [{
+        lat: 50.08770,
+        lng: 14.42079
+    }]
+    },
+
+    {
+    placeName: "Victorious Curch",
+    LatLng: [{
+        lat: 50.08612,
+        lng: 14.40337
+    }]
+    },
+            
+    {
+    placeName: "Charles Bridge",
+    LatLng: [{
+        lat: 50.08675,
+        lng: 14.41141
+    }]
+    },
         
-        ];
+];
 
-        window.onload = function () {
-            initMap();
-        };
 
-        function addMarkerInfo() {
-            for (var i = 0; i < markersOnMap.length; i++) {
-                var contentString = '<div id="content"><h1>' + markersOnMap[i].placeName +
-                    '</h1></div>';
+window.onload = function () {
+    initMap();
+};
 
-                const marker = new google.maps.Marker({
-                    position: markersOnMap[i].LatLng[0],
-                    map: map
-                });
+        
+function addMarkerInfo() {
+    for (var i = 0; i < markersOnMap.length; i++) {
+        var contentString = '<div id="content"><h1>' + markersOnMap[i].placeName +
+        '</h1></div>';
 
-                const infowindow = new google.maps.InfoWindow({
-                    content: contentString,
-                    maxWidth: 200
-                });
+const marker = new google.maps.Marker({
+    position: markersOnMap[i].LatLng[0],
+        map: map
+    });
 
-                marker.addListener('click', function () {
-                    closeOtherInfo();
-                    infowindow.open(marker.get('map'), marker);
-                    InforObj[0] = infowindow;
-                });
+const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+    maxWidth: 200
+    });
+
+marker.addListener('click', function () {
+    closeOtherInfo();
+    infowindow.open(marker.get('map'), marker);
+        InforObj[0] = infowindow;
+    });
                 
-            }
-        }
+    }
+}
 
-        function closeOtherInfo() {
-            if (InforObj.length > 0) {
-                /* detach the info-window from the marker ... undocumented in the API docs */
-                InforObj[0].set("marker", null);
-                /* and close it */
-                InforObj[0].close();
-                /* blank the array */
-                InforObj.length = 0;
-            }
-        }
+function closeOtherInfo() {
+    if (InforObj.length > 0) {
+    /* detach the info-window from the marker ... undocumented in the API docs */
+        InforObj[0].set("marker", null);
+        /* and close it */
+        InforObj[0].close();
+        /* blank the array */
+        InforObj.length = 0;
+    }
+}
 
-        function initMap() {
-            map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 15,
-                center: centerCords
-            });
-            addMarkerInfo();
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+            center: centerCords
+    });
+        addMarkerInfo();
         }
 
 
